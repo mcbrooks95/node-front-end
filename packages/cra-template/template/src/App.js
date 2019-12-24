98f9d15b0c7f090649bbc34350f072dcea5cf554
Mark Brooks <mcbrooks95@gmail.com>
2019-12-23 23:05:39 -0600

    got put to work

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 var url = 'https://limitless-springs-00633.herokuapp.com/books';
 
+// source code = https://www.youtube.com/watch?v=oQnojIyTXb8
+
 export class App extends React.Component {
   state = {
     persons: [],
@@ -19,31 +21,61 @@ export class App extends React.Component {
       });
   }
 
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      title: "cors title",
+      author: "this.state.name",
+      numberPages: 3432,
+      publisher: "test publisher"
+    }
+
+    axios.put("https://limitless-springs-00633.herokuapp.com/book/5dec1fa2655c3e3accfe4803", user)
+    .then((res) => {
+        console.log(res)
+        console.log(res.data)
+    })
+
+  }
+
   render() {
     return (
-      // <ul>
-      //   {this.state.persons.map(person => (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Person Name:
+          <input type="text" name="name" onChange={this.handleChange} />
+        </label>
+        <button type="submit">Add</button>
+      </form>
 
-      //     <li>{person.title}</li>
-      //     // <li>{person.author}</li>
+
+
+
+      // <table style={{ width: '100%' }}>
+      //   <tr>
+      //     <th>title</th>
+      //     <th>author</th>
+      //     <th>numberPages</th>
+      //     <th>publisher</th>
+      //   </tr>
+      //   {this.state.persons.map(person => (
+      //     <tr>
+      //       <td>{person.title}</td>
+      //       <td>{person.author}</td>
+      //       <td>{person.numberPages}</td>
+      //       <td>{person.publisher}</td>
+      //     </tr>
       //   ))}
-      // </ul>
-      <table style={{ width: '100%' }}>
-        <tr>
-          <th>title</th>
-          <th>author</th>
-          <th>numberPages</th>
-          <th>publisher</th>
-        </tr>
-        {this.state.persons.map(person => (
-          <tr>
-            <td>{person.title}</td>
-            <td>{person.author}</td>
-            <td>{person.numberPages}</td>
-            <td>{person.publisher}</td>
-          </tr>
-        ))}
-      </table>
+      // </table>
+
+
+
+
     );
   }
 }
