028aaf01172108763de00b51756764ca2c0be58c
Mark Brooks <mcbrooks95@gmail.com>
2019-12-23 19:56:30 -0600

    cleaned up comments

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -11,27 +11,9 @@ export class App extends React.Component {
   };
 
   componentDidMount() {
-    // axios.get(`https://jsonplaceholder.typicode.com/users`)
     axios
       .get(`https://limitless-springs-00633.herokuapp.com/books`)
-      // axios.get(`http://localhost:5000/books`)
       .then(res => {
-        console.log(`about to print res`);
-        console.log(res);
-        console.log(`about to print res.data`);
-        console.log(res.data);
-
-        res.data.forEach((item, index) => {
-          console.log('item.title = ');
-          console.log(item.title);
-          console.log('item.author = ');
-          console.log(item.author);
-          console.log('item.numberPages = ');
-          console.log(item.numberPages);
-          console.log('item.publisher = ');
-          console.log(item.publisher);
-        });
-
         const persons = res.data;
         this.setState({ persons });
       });
@@ -61,16 +43,6 @@ export class App extends React.Component {
             <td>{person.publisher}</td>
           </tr>
         ))}
-        {/* <tr>
-          <td>Jill</td>
-          <td>Smith</td>
-          <td>50</td>
-        </tr>
-        <tr>
-          <td>Eve</td>
-          <td>Jackson</td>
-          <td>94</td>
-        </tr> */}
       </table>
     );
   }
