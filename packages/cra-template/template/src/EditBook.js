977140b79b1098c7f8d2f863dd90de1a61aaa65d
Mark Brooks <mcbrooks95@gmail.com>
2019-12-24 13:43:20 -0600

    added front end validation

--- packages/cra-template/template/src/EditBook.js
+++ packages/cra-template/template/src/EditBook.js
@@ -25,17 +25,82 @@ export class EditBook extends React.Component {
       });
   }
 
+  isTitleValid = () => {
+      return this.state.title && typeof this.state.title === 'string' && this.state.title.length > 0;
+  }
+
+  isAuthorValid = () => {
+    return this.state.author && typeof this.state.author === 'string' && this.state.author.length > 0;
+  }
+
+  isNumberPagesValid = () => {
+    return this.state.numberPages && typeof parseInt(this.state.numberPages) === 'number'
+             && parseInt(this.state.numberPages) > 0;
+  }
+
+  isPublisherValid = () => {
+      return this.state.publisher && typeof this.state.publisher === 'string' && this.state.publisher.length > 0;
+  }
+
+  isEventTargetValid = (value, type) => {
+      return value 
+      && 
+
+      (type == "string" ? 
+      typeof value === 'string'
+       : typeof parseInt(value) === "number")
+
+       && value.length > 0
+  }
+
   handleChangeTitle = event => {
     this.setState({ title: event.target.value });
+    if( 
+        this.isAuthorValid()
+        && this.isNumberPagesValid()
+        && this.isPublisherValid()
+        && this.isEventTargetValid(event.target.value, "string")
+    )
+    {
+        this.setState({ isAddValid: true });
+    }
   }
   handleChangeAuthor = event => {
     this.setState({ author: event.target.value });
+    if( 
+        this.isTitleValid()
+        && this.isNumberPagesValid()
+        && this.isPublisherValid()
+        && this.isEventTargetValid(event.target.value, "string")
+    )
+    {
+        this.setState({ isAddValid: true });
+    }
   }
   handleChangeNumberPages = event => {
-    this.setState({ numberPages: event.target.value });
+    this.setState({ numberPages: parseInt(event.target.value) });
+    if( 
+        this.isTitleValid()
+        && this.isAuthorValid()
+        && this.isPublisherValid()
+        && this.isEventTargetValid(event.target.value, "number")
+    )
+    {
+        this.setState({ isAddValid: true });
+    }
   }
   handleChangePublisher = event => {
     this.setState({ publisher: event.target.value });
+    if( 
+        this.isTitleValid()
+        && this.isAuthorValid()
+        && this.isNumberPagesValid()
+        && this.isEventTargetValid(event.target.value, "string")
+    )
+    {
+        this.setState({ isAddValid: true });
+    }
+
   }
 
   handleSubmit = event => {
@@ -70,7 +135,7 @@ export class EditBook extends React.Component {
           <input type="text" name="numberPages" onChange={this.handleChangeNumberPages} />
           <input type="text" name="publisher" onChange={this.handleChangePublisher} />
         </label>
-        <button type="submit" style={{
+        <button type="submit" data-isAddValid={this.state.isAddValid} style={{
           
           pointerEvents: (this.state.isAddValid ? "initial" : "none"),
           color: (this.state.isAddValid ? "black" : "gray")
