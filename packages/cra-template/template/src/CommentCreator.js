8b0386dfcd05111d8632e281ad13c6ceb4b4a8d9
Mark Brooks <mcbrooks95@gmail.com>
2020-01-20 17:31:53 -0600

    removed unnecessary code

--- packages/cra-template/template/src/CommentCreator.js
+++ packages/cra-template/template/src/CommentCreator.js
@@ -15,27 +15,18 @@ export class CommentCreator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
+      comment: '',
       value2: '',
       value3: 'interesting',
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleChange2 = this.handleChange2.bind(this);
-    this.handleChange3 = this.handleChange3.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-  handleChange2(event) {
     console.log('in handleChange2 event');
-    this.setState({ value2: event.target.value });
-  }
-  handleChange3(event) {
-    console.log('in handleChange3 event');
-    this.setState({ value3: event.target.value });
+    this.setState({ comment: event.target.value });
   }
 
   handleSubmit(event) {
@@ -43,7 +34,7 @@ export class CommentCreator extends React.Component {
 
     axios
       .post('https://limitless-springs-00633.herokuapp.com/comment', {
-        content: this.state.value2,
+        content: this.state.comment,
         postId: this.props.postId,
         replyingToCommentId: '',
         datePosted: Date.now(),
@@ -52,7 +43,6 @@ export class CommentCreator extends React.Component {
         downvotes: [],
       })
       .then(function(response) {
-        window.location = window.location;
         console.log(response);
       })
       .catch(function(error) {
@@ -67,28 +57,13 @@ export class CommentCreator extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Title of Post:
+          Comment:
           <input
             type="text"
-            value={this.state.value}
+            value={this.state.comment}
             onChange={this.handleChange}
           />
-          Post Content:
-          <input
-            type="text"
-            value={this.state.value2}
-            onChange={this.handleChange2}
-          />
         </label>
-        <select
-          id="lang"
-          onChange={this.handleChange3}
-          value={this.state.value3}
-        >
-          <option value="interesting">interesting</option>
-          <option value="funny">funny</option>
-          <option value="sports">sports</option>
-        </select>
         <input type="submit" value="Submit" />
       </form>
     );
